8d784b3337b4c2442daf32457cfb2b1615fdd3b1
Adam Brewer <adamhb321@gmail.com>
2021-12-04 15:53:24 -0500

    repl

--- NuInfoSys/repl.py
+++ NuInfoSys/repl.py
@@ -4,10 +4,10 @@ from NuInfoSys.framecontrolbytes import *
 
 HELP_MESSAGE: str = '''NuInfoSys REPL Usage:
     [CONFIGURATION]
-    RECEIVING={True, False} - Sets whether or not the REPL should wait for a response from the sign after sending a
-    command. If set to True, the program will wait until it receives data back from the sign OR after the 5 second
-    timeout duration expires.
-    '''
+    RECEIVE={True, False} - Sets whether or not the REPL should wait for a
+    response from the sign after sending a command. If set to True, the
+    program will wait until it receives data back from the sign OR after the
+    5 second timeout duration expires.'''
 
 
 def main():
@@ -16,9 +16,12 @@ def main():
     print(HELP_MESSAGE)
     while True:
         command: str = input(f"[RECEIVE MODE: {receive_mode}]")
+        if "RECEIVE=" in command:
+            choice: str = command.split('=')[1].lower()
+            receive_mode: bool = True if choice == "true" else False
         if receive_mode:
             received: bytes = ser.read_until(PacketCharacter.EOT)
-            print(f"{received}")
+            print(f"RECEIVED FROM SIGN: {received}")
         ser.write(command)
 
 
