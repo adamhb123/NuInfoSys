7cb617987ac03d999ec0f2113b4423692316c3a8
Adam Brewer <adamhb321@gmail.com>
2021-12-04 16:34:18 -0500

    REPL

--- NuInfoSys/repl.py
+++ NuInfoSys/repl.py
@@ -21,7 +21,7 @@ def main():
             choice: str = command.split('=')[1].lower()
             receive_mode: bool = True if choice == "true" else False
         # Send command
-        ser.write(PacketCharacter.NUL * 5 + PacketCharacter.SOH + SignType.SIGN_TYPE_ALL_VERIFY +
+        ser.write(PacketCharacter.NUL * 5 + PacketCharacter.SOH + SignType.SIGN_TYPE_ALL_VERIFY + PacketCharacter.STX +
                   SignAddress.SIGN_ADDRESS_BROADCAST + bytes(command, "utf-8") + PacketCharacter.EOT)
         # Wait for response if in receive mode
         if receive_mode:
